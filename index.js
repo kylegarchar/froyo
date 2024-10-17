369129803ee04dd326e167deb15ccbe5b7c8a4da
Kyle Garchar <kylegarchar@gmail.com>
2024-10-17 09:58:52 -0500

    update code

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// function counts the flavoris in the listed array
+// for eaach flavor in flavors
+// if a flavor exists, increase the flavor count by 1
+// otherwise set the flavor count to 1
+// return the count
 function countFlavors(flavors) {
   const counts = {};
   flavors.forEach(flavor => {
@@ -10,6 +15,11 @@ function countFlavors(flavors) {
   return counts;
 }
 
+// Function to get flavors input from the user and display the counts
+// set the flavorInput to be a prompt for the user and store it
+// split the flavorInput by commas and store the result as flavorCounts
+// return the falvorCounts to the console
+
 function getFlavors() {
   const flavorInput = prompt("Please enter a list of comma-separated froyo flavors:");
   const flavors = flavorInput.split(',');
@@ -17,4 +27,5 @@ function getFlavors() {
   console.log(flavorCounts);
 }
 
+// execute the function
 getFlavors();
